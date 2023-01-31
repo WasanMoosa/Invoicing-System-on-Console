8b4723776c925bbc339818693e9b12f11eecdb8e
WasanMoosa <wasssoon@gmail.com>
2023-01-31 12:19:07 +0400

    add temporarely class to read date

--- src/main/java/com/invoice/system/Invoice.java
+++ src/main/java/com/invoice/system/Invoice.java
@@ -7,60 +7,70 @@ package com.invoice.system;
  * @author Wasan
  *
  */
+import java.util.Date;
+
 public class Invoice {
-	   private String partNumber;
-	   private String partDescription;
-	   private int quantity;
-	   private double pricePerItem;
-	   
-	   public Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
-	      this.partNumber = partNumber;
-	      this.partDescription = partDescription;
-	      this.quantity = quantity;
-	      this.pricePerItem = pricePerItem;
-	   }
-	   
-	   public void setPartNumber(String partNumber) {
-	      this.partNumber = partNumber;
-	   }
-	   
-	   public String getPartNumber() {
-	      return partNumber;
-	   }
-	   
-	   public void setPartDescription(String partDescription) {
-	      this.partDescription = partDescription;
-	   }
-	   
-	   public String getPartDescription() {
-	      return partDescription;
-	   }
-	   
-	   public void setQuantity(int quantity) {
-	      if (quantity < 0) {
-	         this.quantity = 0;
-	      } else {
-	         this.quantity = quantity;
-	      }
-	   }
-	   
-	   public int getQuantity() {
-	      return quantity;
-	   }
-	   
-	   public void setPricePerItem(double pricePerItem) {
-	      if (pricePerItem < 0) {
-	         this.pricePerItem = 0.0;
-	      } else {
-	         this.pricePerItem = pricePerItem;
-	      }
-	   }
-	   
-	   public double getPricePerItem() {
-	      return pricePerItem;
-	   }
-	   
-	   public double getInvoiceAmount() {
-	      return quantity * pricePerItem;
-	   }
-	}
+  private String invoiceNo;
+  private Date invoiceDate;
+  private String customerName;
+  private int noOfItems;
+  private double total;
+  private double balance;
+
+  public Invoice(String invoiceNo, Date invoiceDate, String customerName, int noOfItems, double total, double balance) {
+    this.invoiceNo = invoiceNo;
+    this.invoiceDate = invoiceDate;
+    this.customerName = customerName;
+    this.noOfItems = noOfItems;
+    this.total = total;
+    this.balance = balance;
+  }
+
+  public String getInvoiceNo() {
+    return invoiceNo;
+  }
+
+  public void setInvoiceNo(String invoiceNo) {
+    this.invoiceNo = invoiceNo;
+  }
+
+  public Date getInvoiceDate() {
+    return invoiceDate;
+  }
+
+  public void setInvoiceDate(Date invoiceDate) {
+    this.invoiceDate = invoiceDate;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public int getNoOfItems() {
+    return noOfItems;
+  }
+
+  public void setNoOfItems(int noOfItems) {
+    this.noOfItems = noOfItems;
+  }
+
+  public double getTotal() {
+    return total;
+  }
+
+  public void setTotal(double total) {
+    this.total = total;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+}
---
+++ src/main/java/com/invoice/system/temporarelytest.java
@@ -0,0 +1,12 @@
+package com.invoice.system;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class temporarelytest {
+public static void main(String[] args) {
+Calendar calendar = Calendar.getInstance();
+Date date = calendar.getTime();
+System.out.println("Current Date: " + date);
+}
+}
